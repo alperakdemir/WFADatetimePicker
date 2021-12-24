028a810af94124f9e2ff642140a89d36dba3406e
alperakdemir <alperakdemir004@gmail.com>
2021-12-24 17:11:16 +0300

    burc eklendi

--- WFADatetimePicker/Form1.cs
+++ WFADatetimePicker/Form1.cs
@@ -24,6 +24,65 @@ namespace WFADatetimePicker
             dogumTarihi = dtpDogumTarihi.Value;
             lblYasiniz.Text = "Yaşınız: " + YasHesapla(dogumTarihi.Year);
             lblYasadiginGun.Text = "Yaşadığın gün: " + GunHesapla(dogumTarihi);
+            lblBurc.Text = "Burcunuz: " + BurcHesapla(dogumTarihi.Month, dogumTarihi.Day);
+        }
+
+        private string BurcHesapla(int month, int day)
+        {
+            string burc = "";
+            if ((month == 1 && day >= 21) || (month == 2 && day <= 19))//Kova => Ocak 21 - 19 Şubat
+            {
+                burc = "Kova";
+            }
+            else if ((month == 2 && day >= 20) || (month == 3 && day <= 20))
+            {
+                burc = "Balık";
+            }
+            else if ((month == 3 && day >=21) || (month == 4 && day <= 21))
+            {
+                burc = "Koç";
+            }
+            else if ((month == 4 && day >=21) || (month == 5 && day <= 21))
+            {
+                burc = "Boğa";
+            }
+            else if ((month == 5 && day >=22) || (month == 6 && day <= 21))
+            {
+                burc = "İkizler";
+            }
+            else if ((month == 6 && day >=22) || (month == 7 && day <= 21))
+            {
+                burc = "Yengeç";
+            }
+            else if ((month == 7 && day >= 24) || (month == 8 && day <= 23))
+            {
+                burc = "Aslan";
+            }
+            else if ((month == 8 && day >= 21) || (month == 9 && day <= 23))
+            {
+                burc = "Başak";
+            }
+            else if ((month == 9 && day >= 23) || (month == 10 && day <= 23))
+            {
+                burc = "Terazi";
+            }
+            else if ((month == 10 && day >= 24) || (month == 11 && day <= 22))
+            {
+                burc = "Akrep";
+            }
+            else if ((month == 11 && day >= 23) || (month == 12 && day <= 23))
+            {
+                burc = "Yay";
+            }
+            else if ((month == 4 && day >= 21) || (month == 5 && day <= 21))
+            {
+                burc = "Boğa";
+            }
+            else if ((month == 22 && day >= 12) || (month == 1 && day <= 20))
+            {
+                burc = "Boğa";
+            }
+            return burc;
         }
 
         private string GunHesapla(DateTime dogumTarihi)
