3a7950b048c0040482a2700a7976e6935e063274
alper <alper@DESKTOP-E53EMRP>
2021-12-24 15:50:15 +0300

    Add project files.

---
+++ WFADatetimePicker/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace WFADatetimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblYasiniz = new System.Windows.Forms.Label();
+            this.lblYasadiginGun = new System.Windows.Forms.Label();
+            this.lblBurc = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(176, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğum Tarihiniz";
+            // 
+            // lblYasiniz
+            // 
+            this.lblYasiniz.AutoSize = true;
+            this.lblYasiniz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYasiniz.Location = new System.Drawing.Point(177, 237);
+            this.lblYasiniz.Name = "lblYasiniz";
+            this.lblYasiniz.Size = new System.Drawing.Size(77, 20);
+            this.lblYasiniz.TabIndex = 0;
+            this.lblYasiniz.Text = "Yaşınız: ";
+            // 
+            // lblYasadiginGun
+            // 
+            this.lblYasadiginGun.AutoSize = true;
+            this.lblYasadiginGun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYasadiginGun.Location = new System.Drawing.Point(177, 286);
+            this.lblYasadiginGun.Name = "lblYasadiginGun";
+            this.lblYasadiginGun.Size = new System.Drawing.Size(128, 20);
+            this.lblYasadiginGun.TabIndex = 0;
+            this.lblYasadiginGun.Text = "Yaşadığın gün:";
+            // 
+            // lblBurc
+            // 
+            this.lblBurc.AutoSize = true;
+            this.lblBurc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBurc.Location = new System.Drawing.Point(177, 336);
+            this.lblBurc.Name = "lblBurc";
+            this.lblBurc.Size = new System.Drawing.Size(51, 20);
+            this.lblBurc.TabIndex = 0;
+            this.lblBurc.Text = "Burç:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(181, 75);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(181, 138);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(200, 45);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 442);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblBurc);
+            this.Controls.Add(this.lblYasadiginGun);
+            this.Controls.Add(this.lblYasiniz);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblYasiniz;
+        private System.Windows.Forms.Label lblYasadiginGun;
+        private System.Windows.Forms.Label lblBurc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
